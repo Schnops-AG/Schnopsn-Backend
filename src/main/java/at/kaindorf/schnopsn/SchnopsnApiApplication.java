bdbc97bf64d5575d1ca6c68e8f106e4716f6e1ea
Elias <wileld17@htl-kaindorf.ac.at>
2021-03-16 11:26:08 +0100

    initialized project w(ith spring-boot initializr)

---
+++ src/main/java/at/kaindorf/schnopsn/SchnopsnApiApplication.java
@@ -0,0 +1,13 @@
+package at.kaindorf.schnopsn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchnopsnApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchnopsnApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/kaindorf/schnopsn/SchnopsnApiApplicationTests.java
@@ -0,0 +1,13 @@
+package at.kaindorf.schnopsn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchnopsnApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
