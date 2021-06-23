34f3fa7dde78005e5a850f4a4f692632f1d2cfae
Thomas <kaithd17@htl-kaindorf.at>
2021-06-23 13:06:45 +0200

    JUnit tests

---
+++ src/main/java/at/kaindorf/schnopsn/bl/GameLogicTestClass.java
@@ -0,0 +1,68 @@
+package at.kaindorf.schnopsn.bl;
+
+import at.kaindorf.schnopsn.beans.Card;
+import at.kaindorf.schnopsn.beans.Color;
+import at.kaindorf.schnopsn.beans.Game;
+import at.kaindorf.schnopsn.beans.Player;
+
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class GameLogicTestClass {
+
+    //When someone wants to call a 20er or 40er
+    public String makeCall2erSchnopsn(List<Card> handCards, Color currentTrump) {
+       // List<Card> handCards = game.getPlayerCardMap().get(player);
+        //Becomes true if the first card is found
+        boolean foundFirstCard = false;
+        //To store the first card if it is found
+        Card tempCard = null;
+
+        //alle Colors der Handkarten hinzufügen
+        Set<Color> colorSet = new TreeSet<>();
+        for (Card card : handCards) {
+            card.setPriority(false);
+            colorSet.add(card.getColor());
+        }
+        for (Color color : colorSet) {
+            for (Card card : handCards) {
+                //adde erste Karte der Farbe
+                if ((card.getName().equals("Dame") || card.getName().equals("König")) && card.getColor() == color && !foundFirstCard) {
+                    foundFirstCard = true;
+                    tempCard = card;
+                    //Wenn erste Karte gefunden und diese die gleiche farbe hat
+                } else if (foundFirstCard && card.getColor() == color) {
+                    switch (tempCard.getName()) {
+                        case "Dame":
+                            if (card.getName().equals("König")) {
+                                tempCard.setPriority(true);
+                                card.setPriority(true);
+                                if (currentTrump == color) {
+                                    return "40er";
+                                }
+                                return "20er";
+                            }
+                            break;
+                        case "König":
+                            if (card.getName().equals("Dame")) {
+                                tempCard.setPriority(true);
+                                card.setPriority(true);
+                                if (currentTrump == color) {
+                                    return "40er";
+                                }
+                                return "20er";
+                            }
+                            break;
+                    }
+                }
+            }
+            foundFirstCard = false;
+        }
+
+        for (Card card : handCards) {
+            card.setPriority(true);
+        }
+        return "";
+    }
+}
---
+++ src/test/java/at/kaindorf/schnopsn/GameLogicTest.java
@@ -0,0 +1,22 @@
+package at.kaindorf.schnopsn;
+
+
+import at.kaindorf.schnopsn.bl.GameLogic;
+import at.kaindorf.schnopsn.bl.GameLogicTestClass;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameLogicTest {
+    GameLogicTestClass gltc = new GameLogicTestClass();
+    /*@DisplayName("GameLogic Test")
+    @Test
+    public void test(){
+        int a = 6, b = 2;
+        assertEquals(2, logic.test(a,b));
+    }*/
+
+    public void makeCall2erSchnopsnTest(){
+
+    }
+}
